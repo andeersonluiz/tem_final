6b7dd650c68f99358fd45873d88792a1a6135a4d
Anderson Luiz Bernardes da Rocha <andeersonluiz1998@gmail.com>
2023-03-02 20:26:41 -0300

    last changes for realease

--- lib/src/config/keys.dart
+++
@@ -1,6 +0,0 @@
-const String keySupabase =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltcW10dGF2dWNoZ3J2ZXJ6dmVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzYyNDY2ODgsImV4cCI6MTk5MTgyMjY4OH0.hl6Rw7phI4wwaeEEk4LJB3SMnEOn1XcjTBnu-PQQ4ko";
-const String urlSupabase = "https://imqmttavuchgrverzvee.supabase.co";
-const String cryptoKey = "&a=bZ-AXayakaehutaomainB-=a=#bXY";
-const String banerAdKey = "ca-app-pub-9717626120167653/8243771155";
-String x = "";
